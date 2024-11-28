bbf8cc59f7ef3cfc67f12315c71dd65bfcf08aae
Hu9o73 <hbonnell@live.fr>
2024-11-28 16:10:46 +0100

    Added getFollowers, getFollowing.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Instagram_Tools
+
 Useful tools for instagram ! Relies on your data, not the API.
+
+## How to retrieve my Instagram data ?
+- Parameters
+- Meta account
+- Informations and authorizations
+- Download your informations
+- Download or transfer your informations
+- All info / Some info depending on your usage. If in doubt : All info
+- Download on device
+- Period: Since the beining
+- Format: HTML
+- Create files !
+
+Then you must wait for the files to be ready and download the zipped folder. Unzip it, its name should start with: instagram-username.
+
+That's the file you have to refer to when asked for a filepath in somefunctions !
---
+++ utils.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+
+
+def getFollowers(file="/"):
+    '''
+    Retrieve the list of your followers as an array.\n
+    Pass the filepath to your instagram's data (relative to this script). (instagram-username folder)\n
+    Don't forget the "/" at the end of the filename.
+    '''
+    file_path = file + "connections/followers_and_following/followers_1.html"
+    with open(file_path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+    soup = BeautifulSoup(html_content, 'html.parser')
+    a6_p_divs = soup.find_all('div', class_='_a6-p')
+    names = [div.find('a').text for div in a6_p_divs if div.find('a')]
+
+    return names
+
+def getFollowing():
+    '''
+    Retrieve the list of the people you follow as an array.\n
+    Pass the filepath to your instagram's data (relative to this script). (instagram-username folder)\n
+    Don't forget the "/" at the end of the filename.
+    '''
+    file_path = file + "connections/followers_and_following/following.html"
+    with open(file_path, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+    soup = BeautifulSoup(html_content, 'html.parser')
+    a6_p_divs = soup.find_all('div', class_='_a6-p')
+    names = [div.find('a').text for div in a6_p_divs if div.find('a')]
+
+    return names
+

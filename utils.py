8b49419573fa995e8922dc685832fe2aaf60f663
Hu9o73 <hbonnell@live.fr>
2024-11-28 16:26:52 +0100

    Update to ease the access to the data folder. Added Example folder.

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 
 
-def getFollowers(file="/"):
+def getFollowers(dir="/"):
     '''
     Retrieve the list of your followers as an array.\n
     Pass the filepath to your instagram's data (relative to this script). (instagram-username folder)\n
     Don't forget the "/" at the end of the filename.
     '''
-    file_path = file + "connections/followers_and_following/followers_1.html"
+    file_path = dir + "connections/followers_and_following/followers_1.html"
     with open(file_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
     soup = BeautifulSoup(html_content, 'html.parser')
@@ -16,13 +16,13 @@ def getFollowers(file="/"):
 
     return names
 
-def getFollowing():
+def getFollowing(dir="/"):
     '''
     Retrieve the list of the people you follow as an array.\n
     Pass the filepath to your instagram's data (relative to this script). (instagram-username folder)\n
     Don't forget the "/" at the end of the filename.
     '''
-    file_path = file + "connections/followers_and_following/following.html"
+    file_path = dir + "connections/followers_and_following/following.html"
     with open(file_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
     soup = BeautifulSoup(html_content, 'html.parser')
